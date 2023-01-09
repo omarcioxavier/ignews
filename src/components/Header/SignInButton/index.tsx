e3b947436f63b087142dc5c6970fafdee364ba9d
Márcio Xavier <juniorxavier606@gmail.com>
2023-01-09 20:20:00 -0300

    verificar usuario ja cadastrado

--- src/components/Header/SignInButton/index.tsx
+++ src/components/Header/SignInButton/index.tsx
@@ -6,8 +6,6 @@ import { useSession, signIn, signOut } from "next-auth/react"
 export default function SignInButton() {
     const { data: session } = useSession();
 
-    if (session) { console.log(session); }
-
     return session ? (
         <button type="button" className={styles.signInButton} onClick={() => signOut()}>
             <FaGithub color="#04d361" />
