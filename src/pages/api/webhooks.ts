391cbfbcf71f81fadf012d35c8ba67cd0dc31378
Márcio Xavier <juniorxavier606@gmail.com>
2023-01-17 21:29:16 -0300

    ouvindo + eventos do stripe
    Eventos como inscrição cancelada / atualizada

--- src/pages/api/webhooks.ts
+++ src/pages/api/webhooks.ts
@@ -2,6 +2,7 @@ import { NextApiRequest, NextApiResponse } from "next";
 import { Readable } from "stream";
 import Stripe from "stripe";
 import { stripe } from "../../services/stripe";
+import { saveSubscription } from "./_lib/manageSubscription";
 
 async function buffer(readable: Readable) {
     const chunks = [];
@@ -20,7 +21,9 @@ export const config = {
 }
 
 const relevantEvents = new Set([
-    "checkout.session.completed"
+    "checkout.session.completed",
+    "customer.subscription.updated",
+    "customer.subscription.deleted"
 ]);
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
@@ -44,6 +47,23 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
     const { type } = event;
 
     if (relevantEvents.has(type)) {
+        try {
+            switch (type) {
+                case "checkout.session.completed":
+                    const checkoutSession = event.data.object as Stripe.Checkout.Session;
+                    saveSubscription(checkoutSession.subscription?.toString() ?? "", checkoutSession.customer?.toString() ?? "", true);
+                    break;
+                case "customer.subscription.updated":
+                case "customer.subscription.deleted":
+                    const subscription = event.data.object as Stripe.Subscription;
+                    await saveSubscription(subscription.id.toString() ?? "", subscription.customer.toString());
+                    break;
+                default:
+                    throw new Error("Unhandled event");
+            }
+        } catch (error) {
+
+        }
         console.log("Evento recebido: ", event);
     }
 
